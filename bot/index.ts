7eea96dfb2fce539eb7d23909cf25e9863026ee6
ozyx <jessemazzella@gmail.com>
2018-05-28 14:06:19 -0700

    finish working example

--- bot/index.ts
+++ bot/index.ts
@@ -1,5 +1,30 @@
+const { OWNERS, BEERBOT_TOKEN } = process.env;
+import * as path from "path";
 import { CommandoClient } from "discord.js-commando";
 
-const client = new CommandoClient({
-    owner: "1234567890"
+// TODO: Find a better way to check if these are undefined.
+if (OWNERS === undefined) {
+    throw new Error("OWNERS environment variable must be defined!");
+}
+
+if (BEERBOT_TOKEN === undefined) {
+    throw new Error("BEERBOT_TOKEN environment variable must be defined!");
+}
+
+const client: CommandoClient = new CommandoClient({
+    owner: OWNERS.split(","),
+});
+
+client.registry.registerDefaults()
+    .registerCommandsIn(path.join(__dirname, "commands"));
+
+client.on("ready", () => {
+    console.log(`[READY] Logged in as ${client.user.tag}! (${client.user.id})`);
+});
+
+client.login(BEERBOT_TOKEN);
+
+process.on("unhandledRejection", err => {
+	console.error("[FATAL] Unhandled Promise Rejection.", err);
+	process.exit(1);
 });
