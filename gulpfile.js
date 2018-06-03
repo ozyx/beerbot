6010782b98204a5fc0537a06256d30b1b9037056
ozyx <jessemazzella@gmail.com>
2018-06-02 20:46:42 -0700

    simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,32 @@ var tsProject = ts.createProject("tsconfig.json");
 var clean = require('gulp-clean');
 var gulp_tslint = require('gulp-tslint');
 
+/**
+ * TASK tslint
+ * Lint all TypeScript files using tslint
+ */
 gulp.task('tslint', () => {
     return gulp.src(['**/*.ts', '!**/*.d.ts', '!node_modules/**', '!bot/tests/**'])
         .pipe(gulp_tslint())
         .pipe(gulp_tslint.report());
 });
 
-gulp.task('clean', function () {
+/**
+ * TASK clean
+ * Remove all transpiled code
+ */
+gulp.task('clean', () => {
     return gulp.src(['bot/**/*.js', 'bot/**/*.js.map'], {
             read: false
         })
         .pipe(clean());
 });
 
-gulp.task("build", gulp.series(['clean', 'tslint'], function () {
+/**
+ * TASK sourcemap
+ * Generate *.map files for debugging
+ */
+gulp.task('sourcemap', () => {
     var tsResult = gulp.src('bot/**/*.ts')
         .pipe(sourcemaps.init()) // This means sourcemaps will be generated
         .pipe(tsProject());
@@ -26,6 +38,15 @@ gulp.task("build", gulp.series(['clean', 'tslint'], function () {
     return tsResult.js
         .pipe(sourcemaps.write('.')) // Now the sourcemaps are added to the .js file
         .pipe(gulp.dest('bot'));
-}));
+});
 
-gulp.task("default", gulp.series(['build'], function (done){done()}));
+/**
+ * TASK build
+ * Clean, run lint, build and generate map files
+ */
+gulp.task("build", gulp.series(['clean', 'tslint', 'sourcemap']));
+
+/**
+ * TASK default
+ */
+gulp.task("default", gulp.task('build'));
