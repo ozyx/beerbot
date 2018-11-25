a17723ada09992d4cc16b8603a5d99031c35a52c
ozyx <jessemazzella@gmail.com>
2018-11-24 22:52:02 -0800

    add experimental ascii thingy

---
+++ bot/commands/fun/ascii.ts
@@ -0,0 +1,32 @@
+import { Message } from "discord.js";
+import { Command, CommandMessage, CommandoClient } from "discord.js-commando";
+import figlet = require("figlet");
+
+export default class AsciiCommand extends Command {
+
+    constructor(client: CommandoClient) {
+        const commandName = "ascii";
+        super(client, {
+            aliases: ["a"],
+            description: "Turn text into Ascii Art!",
+            examples: [`${commandName} {message}`],
+            group: "fun",
+            guildOnly: true,
+            memberName: "ascii",
+            name: commandName,
+        });
+    }
+
+    public async run(message: CommandMessage, args: object): Promise<(Message | Message[])> {
+
+        let finalMessage: string = figlet.textSync(args, {font: "1Row"});
+
+        if (finalMessage === "") {
+            // do something
+        }
+
+        finalMessage = "```" + finalMessage + "```";
+
+        return message.channel.send(finalMessage);
+    }
+}
